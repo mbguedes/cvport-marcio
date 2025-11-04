f91d1f397166a8482020f24f38d3fd737b8bf19a
mbguedes <marcio.bodiniguedes@gmail.com>
2025-11-03 21:50:44 -0300

    Config: Base path para Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path' // 1. Importe o 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cvport-marcio/',
   plugins: [vue()],
   resolve: { // 2. Adicione esta seção 'resolve'
     alias: {
